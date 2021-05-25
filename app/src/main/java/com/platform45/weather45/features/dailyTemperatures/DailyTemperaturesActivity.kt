9a992a5277fae8252ca75282d1cded303598f5cd
Tshepo <rocboyt@gmail.com>
2021-05-25 12:10:23 +0200

    Fixed gradle bug

--- app/src/main/java/com/platform45/weather45/features/dailyTemperatures/DailyTemperaturesActivity.kt
+++ app/src/main/java/com/platform45/weather45/features/dailyTemperatures/DailyTemperaturesActivity.kt
@@ -6,10 +6,15 @@ import com.platform45.weather45.R
 //import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DailyTemperaturesActivity : AppCompatActivity() {
+    // private lateinit var binding: ActivityWeatherBinding
     //private val dailyTemperaturesViewModel: DailyTemperaturesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_temperatures)
+
+        //       binding = DataBindingUtil.setContentView(this, R.layout.activity_weather)
+        //        binding.weatherViewModel = weatherViewModel
+        //        binding.lifecycleOwner = this
     }
 }

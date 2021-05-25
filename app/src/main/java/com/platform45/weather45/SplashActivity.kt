4027b20d22c779a508590baf9ee4b7b7e02b3824
Tshepo <rocboyt@gmail.com>
2021-05-25 08:28:33 +0200

    Added splash

--- app/src/main/java/com/platform45/weather45/SplashActivity.kt
+++ app/src/main/java/com/platform45/weather45/SplashActivity.kt
@@ -3,9 +3,8 @@ package com.platform45.weather45
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }

eb4875e611950c191eb1f7d1573b524c75a241df
Tshepo <rocboyt@gmail.com>
2021-05-25 13:55:22 +0200

    Added colors and themes

--- app/src/main/java/com/platform45/weather45/features/dailyTemperatures/DailyTemperaturesActivity.kt
+++ app/src/main/java/com/platform45/weather45/features/dailyTemperatures/DailyTemperaturesActivity.kt
@@ -2,19 +2,19 @@ package com.platform45.weather45.features.dailyTemperatures
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.platform45.weather45.R
-//import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.platform45.weather45.databinding.ActivityDailyTemperaturesBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DailyTemperaturesActivity : AppCompatActivity() {
-    // private lateinit var binding: ActivityWeatherBinding
-    //private val dailyTemperaturesViewModel: DailyTemperaturesViewModel by viewModel()
+    private lateinit var binding: ActivityDailyTemperaturesBinding
+    private val dailyTemperaturesViewModel: DailyTemperaturesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_daily_temperatures)
-
-        //       binding = DataBindingUtil.setContentView(this, R.layout.activity_weather)
-        //        binding.weatherViewModel = weatherViewModel
-        //        binding.lifecycleOwner = this
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_daily_temperatures)
+        binding.dailyTemperaturesViewModel = dailyTemperaturesViewModel
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/platform45/weather45/features/dailyTemperatures/DailyTemperaturesViewModel.kt
+++ app/src/main/java/com/platform45/weather45/features/dailyTemperatures/DailyTemperaturesViewModel.kt
@@ -1,9 +1,18 @@
 package com.platform45.weather45.features.dailyTemperatures
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import com.platform45.weather45.base.viewmodels.BaseVieModel
 import com.platform45.weather45.repositories.WeatherRepository
 
 class DailyTemperaturesViewModel(application: Application, weatherRepository: WeatherRepository) : BaseVieModel(application) {
 
+    private val _message: MutableLiveData<String> = MutableLiveData()
+    val message: MutableLiveData<String>
+        get() = _message
+
+    init {
+        val dd = weatherRepository
+        _message.value = "Hello world"
+    }
 }

e0b7c6e36d6639748b66f6b8018105c296514d3e
Tshepo <rocboyt@gmail.com>
2021-05-25 11:53:06 +0200

    Added base ViewModel

---
+++ app/src/main/java/com/platform45/weather45/base/viewmodels/BaseVieModel.kt
@@ -0,0 +1,19 @@
+package com.platform45.weather45.base.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+
+abstract class BaseVieModel(application: Application) : AndroidViewModel(application){
+    protected var app = application
+    protected var viewModelJob = Job()
+    protected val ioScope = CoroutineScope(Dispatchers.IO + viewModelJob)
+    protected val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}

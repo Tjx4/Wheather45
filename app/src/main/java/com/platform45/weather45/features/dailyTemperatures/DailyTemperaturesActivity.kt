07615ff529b0da13a9d5afe948ac8cff1782f63d
Tshepo <rocboyt@gmail.com>
2021-05-25 08:42:33 +0200

    added daily temperatures Activity

---
+++ app/src/main/java/com/platform45/weather45/features/dailyTemperatures/DailyTemperaturesActivity.kt
@@ -0,0 +1,12 @@
+package com.platform45.weather45.features.dailyTemperatures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.platform45.weather45.R
+
+class DailyTemperaturesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_temperatures)
+    }
+}
